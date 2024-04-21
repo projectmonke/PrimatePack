67a536afdf512a37a5e6742af1d4aa744bc140c6
Project Monke <132834912+projectmonke@users.noreply.github.com>
2024-04-21 17:45:11 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,195 @@
+// Primate Pack
+
+// Monke Defaults
+var defaultMonkeSettings = {
+    gif_url: "https://media.tenor.com/fAQ4mdg7iz4AAAAj/pixel-cat.gif",
+    gif_width: "200",
+    gif_height: "200",
+    caido_pets_enabled: true
+}
+
+// Get Monke Settings
+var getMonkeSetting = (settingName) => {
+  if (localStorage.getItem(`monke_${settingName}`) === null) {
+    return defaultMonkeSettings[settingName];
+  }
+  return localStorage.getItem(`monke_${settingName}`) || "";
+};
+
+// Set Monke Settings
+var setMonkeSetting = (settingName, value) => {
+  localStorage.setItem(`monke_${settingName}`, value);
+};
+
+// Settings Tab HTML
+var createMonkeTabHTML = () => {
+  const monkeFeatures = [
+    {
+      name: "caido_pets_enabled",
+      title: "Caido Pets",
+      items: [
+        {
+          name: "caido_pets_enabled",
+          title: "Caido Pets",
+          description: "Adds a gif in the bottom-left corner of the Caido UI."
+        }
+      ]
+    }
+  ];
+
+  const featureHtml = monkeFeatures.map(feature => `
+    <div class="toggle-features__content-item">
+      <div class="toggle-features__content-item-title">${feature.title}</div>
+      ${feature.items.map(item => `
+        <div class="toggle-features__content-item-toggle">
+          <div class="toggle-features__content-item-description">
+            <b>${item.title}:</b> ${item.description}
+          </div>
+          <div>
+            <input type="checkbox" name="${item.name}" id="${item.name}" ${getMonkeSetting(item.name) === "true" ? "checked" : ""} />
+          </div>
+        </div>`).join("")}
+    </div>
+    <hr />`).join("");
+
+  return `
+    <div class="even-better__settings" id="evenbetter-settings-content">
+      <header>
+        <div class="header-title"><h1>The Primate Pack - Settings</h1></div>
+        <div class="header-description">Primate Pack settings.</div>
+      </header>
+      <main>
+        <div class="left">
+          <div class="settings-box" id="caidoPets">
+            <div class="settings-box__header">
+              <div class="settings-box__header-title">Caido Pets</div>
+              <div class="settings-box__header-description">Pet Configuration</div>
+            </div>
+            <div class="settings-box__content">
+              <div class="c-text-input">
+                <div class="c-text-input__outer">
+                  <u>GIF URL</u>
+                  <div class="c-text-input__inner">
+                    <input type="text" placeholder="https://www.example.com" spellcheck="false" class="c-text-input__input" id="gif_url" />
+                  </div>
+                </div>
+                <br>
+                <div class="c-text-input__outer">
+                  <u>Width</u>
+                  <div class="c-text-input__inner">
+                    <input type="text" placeholder="200" spellcheck="false" class="c-text-input__input" id="gif_width" />
+                  </div>
+                </div>
+                <br>
+                <div class="c-text-input__outer">
+                  <u>Height</u>
+                  <div class="c-text-input__inner">
+                    <input type="text" placeholder="200" spellcheck="false" class="c-text-input__input" id="gif_height" />
+                  </div>
+                </div>
+              </div>
+              <button id="petSaveButton">Save</button>
+            </div>
+          </div>
+        </div>
+        <div class="right">
+          <div class="toggle-features" id="featureFlags">
+            <div class="toggle-features__header">
+              <div class="toggle-features__header-title">Toggle Features</div>
+              <div class="toggle-features__header-description">Enable or disable features of the Primate Pack plugins</div>
+            </div>
+            <hr />
+            <div class="toggle-features__content">${featureHtml}</div>
+            <button id="featureSaveButton">Save</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  `;
+};
+
+
+// Monke Settings Tab
+var monkeTab = () => {
+  const monkeTabHTML = document.createElement("div");
+  monkeTabHTML.innerHTML = createMonkeTabHTML();
+
+  const monkePetsFunctionality = monkeTabHTML.querySelector("#caidoPets");
+  const featureFlagFunctionality = monkeTabHTML.querySelector("#featureFlags");
+  
+  if (monkePetsFunctionality && featureFlagFunctionality) {
+    const caidoPetsButton = monkePetsFunctionality.querySelector("#petSaveButton");
+    const featureSaveButton = featureFlagFunctionality.querySelector("#featureSaveButton");
+    const inputs = monkePetsFunctionality.querySelectorAll("input");
+    const featureFlagCheckboxes = featureFlagFunctionality.querySelectorAll("input[type='checkbox']");
+
+    inputs.forEach(input => input.addEventListener("input", () => caidoPetsButton.disabled = false));
+
+    featureFlagCheckboxes.forEach(checkbox => {
+      checkbox.addEventListener("change", () => {
+        featureSaveButton.disabled = false;
+      });
+    });
+
+    caidoPetsButton.addEventListener("click", () => {
+      ["gif_url", "gif_width", "gif_height"].forEach((item, idx) => setMonkeSetting(item, inputs[idx].value));
+      location.reload();
+    });
+
+    featureSaveButton.addEventListener("click", () => {
+      featureFlagCheckboxes.forEach((checkbox) => {
+        setMonkeSetting(checkbox.name, checkbox.checked);
+      });
+      location.reload();
+    });
+
+    inputs.forEach((input, idx) => {
+      if (idx < inputs.length) input.value = getMonkeSetting(input.id);
+    });
+
+    featureFlagCheckboxes.forEach(checkbox => {
+      checkbox.checked = getMonkeSetting(checkbox.name) === "true";
+    });
+  }
+
+  return monkeTabHTML;
+};
+
+// Create Pets UI
+var monkeTools = () => {
+
+  if (getMonkeSetting("caido_pets_enabled") === "true"){
+    // Adds the tab
+    const settingsNavigation = document.querySelector(
+      ".c-settings .p-menubar-root-list"
+    );
+    let customTabMonke = {
+      name: "The Primate Pack",
+      icon: '<i class="c-icon fas fa-cat"></i>',
+      id: "monketools",
+      content: monkeTab()
+    }
+
+    settingsNavigation.appendChild(customTabMonke);
+
+
+    Caido.sidebar.registerItem("", "#", {
+      icon: "",
+      group: `<img id='gifImage' width='200' height='200'>`,
+    });
+    setGif();
+  }
+};
+
+var setGif = () => {
+  const gifImageElement = document.getElementById("gifImage");
+  if (gifImageElement) {
+    gifImageElement.src = getMonkeSetting("gif_url");
+    gifImageElement.width = getMonkeSetting("gif_width");
+    gifImageElement.height = getMonkeSetting("gif_height");
+  } else {
+    setTimeout(setGif, 500);
+  }
+};
+
+monkeTools();
