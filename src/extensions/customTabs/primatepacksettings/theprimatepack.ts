b836839baa508ac642c2a816c9eecf31d44736c3
Ciarán Cotter <ciaran@surfacewatch.io>
2024-05-18 18:35:25 +0900

    Added activity tracker outline

--- src/extensions/customTabs/primatepacksettings/theprimatepack.ts
+++ src/extensions/customTabs/primatepacksettings/theprimatepack.ts
@@ -44,83 +44,53 @@ const primatePackSettingsTabHTML = () => {
       ],
     },
     {
-      name: "notion_enabled",
-      title: "Notion",
+      name: "activity_tracker_enabled",
+      title: "Activity Tracker",
       items: [
         {
-          name: "notion_enabled",
-          title: "Notion",
-          description: "Enable Notion within Caido.",
+          name: "activity_tracker_enabled",
+          title: "Activity Tracker",
+          description: "Enable Activity Tracker within Caido.",
         },
       ],
     },
   ];
 
-  const featureHtml = monkeFeatures.map(feature => `
+  const featureHtml = monkeFeatures
+    .map(
+      (feature) => `
     <div class="toggle-features__content-item">
       <div class="toggle-features__content-item-title">${feature.title}</div>
-      ${feature.items.map(item => `
+      ${feature.items
+        .map(
+          (item) => `
         <div class="toggle-features__content-item-toggle">
           <div class="toggle-features__content-item-description">
             <b>${item.title}:</b> ${item.description}
           </div>
           <div>
-            <input type="checkbox" name="${item.name}" id="${item.name}" ${getMonkeSetting(item.name) === "true" ? "checked" : ""} />
+            <input type="checkbox" name="${item.name}" id="${item.name}" ${
+            getMonkeSetting(item.name) === "true" ? "checked" : ""
+          } />
           </div>
-        </div>`).join("")}
+        </div>`
+        )
+        .join("")}
     </div>
-    <hr />`).join("");
+    <hr />`
+    )
+    .join("");
 
   return `
   <div class="even-better__settings" id="evenbetter-settings-content">
     <header>
-      <div class="header-title"><h1>The Primate Pack - Settings</h1></div>
-      <div class="header-description">Primate Pack settings.</div>
+      <div class="header-title"><h1>Activity Tracker</h1></div>
+      <div class="header-description">Track your hacking activity here.</div>
     </header>
     <main>
       <div class="left">
-        <div class="settings-box" id="caidoPets">
-          <div class="settings-box__header">
-            <div class="settings-box__header-title">Caido Pets</div>
-            <div class="settings-box__header-description">Pet Configuration</div>
-          </div>
-          <div class="settings-box__content">
-            <div class="c-text-input">
-              <div class="c-text-input__outer">
-                <u>GIF URL</u>
-                <div class="c-text-input__inner">
-                  <input type="text" placeholder="https://www.example.com" spellcheck="false" class="c-text-input__input" id="gif_url" />
-                </div>
-              </div>
-              <br>
-              <div class="c-text-input__outer">
-                <u>Width</u>
-                <div class="c-text-input__inner">
-                  <input type="text" placeholder="200" spellcheck="false" class="c-text-input__input" id="gif_width" />
-                </div>
-              </div>
-              <br>
-              <div class="c-text-input__outer">
-                <u>Height</u>
-                <div class="c-text-input__inner">
-                  <input type="text" placeholder="200" spellcheck="false" class="c-text-input__input" id="gif_height" />
-                </div>
-              </div>
-            </div>
-            <button id="petSaveButton">Save</button>
-          </div>
-        </div>
       </div>
       <div class="right">
-        <div class="toggle-features" id="featureFlags">
-          <div class="toggle-features__header">
-            <div class="toggle-features__header-title">Toggle Features</div>
-            <div class="toggle-features__header-description">Enable or disable features of the Primate Pack plugins</div>
-          </div>
-          <hr />
-          <div class="toggle-features__content">${featureHtml}</div>
-          <button id="featureSaveButton">Save</button>
-        </div>
       </div>
     </main>
   </div>
@@ -132,7 +102,7 @@ export const primatePackSettingsTab = () => {
   const container = document.createElement("div");
   container.innerHTML = primatePackSettingsTabHTML();
   const checkboxes = container.querySelectorAll("input[type=checkbox]");
-  checkboxes.forEach(checkbox => {
+  checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", (event) => {
       const target = event.target as HTMLInputElement;
       setMonkeSetting(target.name, target.checked ? "true" : "false");
@@ -141,9 +111,13 @@ export const primatePackSettingsTab = () => {
 
   const petSaveButton = container.querySelector("#petSaveButton");
   petSaveButton.addEventListener("click", () => {
-    const gifUrl = (container.querySelector("#gif_url") as HTMLInputElement).value;
-    const gifWidth = (container.querySelector("#gif_width") as HTMLInputElement).value;
-    const gifHeight = (container.querySelector("#gif_height") as HTMLInputElement).value;
+    const gifUrl = (container.querySelector("#gif_url") as HTMLInputElement)
+      .value;
+    const gifWidth = (container.querySelector("#gif_width") as HTMLInputElement)
+      .value;
+    const gifHeight = (
+      container.querySelector("#gif_height") as HTMLInputElement
+    ).value;
     setMonkeSetting("gif_url", gifUrl);
     setMonkeSetting("gif_width", gifWidth);
     setMonkeSetting("gif_height", gifHeight);
@@ -151,4 +125,3 @@ export const primatePackSettingsTab = () => {
 
   return container;
 };
-
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log("Hello via Bun!");
+// Nothing here!
