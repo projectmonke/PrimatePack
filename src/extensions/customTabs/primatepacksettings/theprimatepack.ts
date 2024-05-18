b431a891ad1751fa2d43a4177d9ca5bd23b18ae5
Ciarán Cotter <ciaran@surfacewatch.io>
2024-05-18 18:43:05 +0900

    Removed wrong code

--- src/extensions/customTabs/primatepacksettings/theprimatepack.ts
+++ src/extensions/customTabs/primatepacksettings/theprimatepack.ts
@@ -84,13 +84,53 @@ const primatePackSettingsTabHTML = () => {
   return `
   <div class="even-better__settings" id="evenbetter-settings-content">
     <header>
-      <div class="header-title"><h1>Activity Tracker</h1></div>
-      <div class="header-description">Track your hacking activity here.</div>
+      <div class="header-title"><h1>The Primate Pack - Settings</h1></div>
+      <div class="header-description">Primate Pack settings.</div>
     </header>
     <main>
       <div class="left">
+        <div class="settings-box" id="caidoPets">
+          <div class="settings-box__header">
+            <div class="settings-box__header-title">Caido Pets</div>
+            <div class="settings-box__header-description">Pet Configuration</div>
+          </div>
+          <div class="settings-box__content">
+            <div class="c-text-input">
+              <div class="c-text-input__outer">
+                <u>GIF URL</u>
+                <div class="c-text-input__inner">
+                  <input type="text" placeholder="https://www.example.com" spellcheck="false" class="c-text-input__input" id="gif_url" />
+                </div>
+              </div>
+              <br>
+              <div class="c-text-input__outer">
+                <u>Width</u>
+                <div class="c-text-input__inner">
+                  <input type="text" placeholder="200" spellcheck="false" class="c-text-input__input" id="gif_width" />
+                </div>
+              </div>
+              <br>
+              <div class="c-text-input__outer">
+                <u>Height</u>
+                <div class="c-text-input__inner">
+                  <input type="text" placeholder="200" spellcheck="false" class="c-text-input__input" id="gif_height" />
+                </div>
+              </div>
+            </div>
+            <button id="petSaveButton">Save</button>
+          </div>
+        </div>
       </div>
       <div class="right">
+        <div class="toggle-features" id="featureFlags">
+          <div class="toggle-features__header">
+            <div class="toggle-features__header-title">Toggle Features</div>
+            <div class="toggle-features__header-description">Enable or disable features of the Primate Pack plugins</div>
+          </div>
+          <hr />
+          <div class="toggle-features__content">${featureHtml}</div>
+          <button id="featureSaveButton">Save</button>
+        </div>
       </div>
     </main>
   </div>
